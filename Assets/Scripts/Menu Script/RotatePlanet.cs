c7106062279529f3ba0569198cf6ab67a8c32156
DiegoTong <44652684+DiegoTong@users.noreply.github.com>
2020-11-24 23:24:05 +0000

    Added UI Elements and Main Menu

---
+++ Assets/Scripts/Menu Script/RotatePlanet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=hd1QzLf4ZH8
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = (target.position + new Vector3(0, 0, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime/2);
+        transform.Translate(0, 0, 3 * Time.deltaTime);
+    }
+}
